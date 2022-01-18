e4f9b80d48b6492aad4822e8f95d72c7578ddb0b
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-01-19 00:52:17 +0900

    코인 가격 나열 완료, 영화 서비스 시작

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react/cjs/react.development";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setTodos] = useState([]); // 기본 값 : 빈 배열
-
-  const onChnage = (e) => setTodo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return; // toDo가 비어있다면, 이 함수가 작동되지 않게 끊어주기위해 바로 return;을 해줌.
-    } else {
-      setTodos((currentArray) => [toDo, ...currentArray]);
-      setTodo(""); // input의 value를 비워주는 값. input value = toDo = setTodo 이어져있기 떄문에 toDo의 값이 ""이 되면 input의 밸류도 ""이 되는 것
-    }
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovie(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    console.log(toDos);
-  }, [toDos]);
+    getMovies();
+  }, []);
+  console.log(movie);
   return (
     <div>
-      <h1>My To Do ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChnage}
-          value={toDo}
-          type="Text"
-          placeholder="할 일을 적어"
-        />
-        <button>Add To do</button>
-      </form>
+      {loading ? (
+        <h1>로딩중 . . .</h1>
+      ) : (
+        <div>
+          {movie.map((item) => (
+            <div key={item.id}>
+              <img src={item.medium_cover_image} />
+              <h2>{item.title}</h2>
+              <p>{item.summary}</p>
+              <ul>
+                {item.genres.map((g) => (
+                  <li key={g}>{g}</li> // map을 쓸 떄마다 key를 한 번씩 줘야함
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
